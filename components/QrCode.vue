179c2ec9ff8bf3711dba660887f57db5f918a19b
Ann Serdukova <404region@gmail.com>
2022-08-16 02:18:07 +0300

    Почистила код

--- components/QrCode.vue
+++ components/QrCode.vue
@@ -5,7 +5,7 @@
         <div class="reader-block reader-block--padding">
             <div id="reader" class="reader"></div>
         </div>
-        <el-button v-if="scanOpen" @click="scanEnd" type="warning" round>Завершить</el-button>
+        <el-button v-if="scanCloseShow" @click="scanEnd" type="warning" round>Завершить</el-button>
         <el-input v-if="qrCodeInputShow" placeholder="Please input" v-model="qrCodeText"></el-input>
     </div>
 </template>
@@ -17,35 +17,33 @@
         name: 'QrCode',
         data() {
             return {
-                scanOpen: false,
+                scanCloseShow: false,
                 qrCodeInputShow: false,
                 qrCodeText: '',
+                html5QrcodeScanner: {}
             }
         },
         methods: {
             myScanSuccess(decodedResult) {
-                console.log('myScanSuccess', decodedResult);
-                this.scanOpen = false;
+                // Скрываем\показываем элементы, отображаем результат
+                this.scanCloseShow = false;
                 this.qrCodeInputShow = true;
                 this.qrCodeText = decodedResult.decodedText;
             },
             scanIt() {
                 let self = this;
-                this.scanOpen = true;
+                //Показываем кнопку Завершить
+                this.scanCloseShow = true;
+                //Скрываем инпут, на случай если нажали сканировать повторно 
                 this.qrCodeInputShow = false;
-                console.log('scan');
                 function onScanSuccess(decodedText, decodedResult) {
                     // handle the scanned code as you like, for example:
-                    console.log(`Code matched = ${decodedText}`, decodedResult);
-                    console.log('еще консоль');
-                    console.log('еще консоль 2 ');
-                    console.log('self', self);
+                    //console.log(`Code matched = ${decodedText}`, decodedResult);
                    
                     self.myScanSuccess(decodedResult);
-                    //console.log('his.scanOpen',this.scanOpen);
                     
                     // Stop scanning
-                    html5QrcodeScanner.clear().then(_ => {
+                    self.html5QrcodeScanner.clear().then(_ => {
                         // the UI should be cleared here 
                     }).catch(error => {
                         // Could not stop scanning for reasons specified in `error`.
@@ -58,21 +56,28 @@
                     //console.warn(`Code scan error = ${error}`);
                 }
 
-                let html5QrcodeScanner = new Html5QrcodeScanner(
+                self.html5QrcodeScanner = new Html5QrcodeScanner(
                     "reader",
-                    { fps: 10, qrbox: {width: 250, height: 250} },
+                    { 
+                        fps: 10,
+                        qrbox: {width: 250, height: 250},
+                    },
                 /* verbose= */ false
                 );
 
-                html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+                self.html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 
             },
             scanEnd() {
-                console.log('scan end');
-                //Пишем в инпут нужный текст
-                //decodedText
-
-
+                //Очищаем сканер
+                this.html5QrcodeScanner.clear().then(_ => {
+                        // the UI should be cleared here 
+                }).catch(error => {
+                    // Could not stop scanning for reasons specified in `error`.
+                    // This conditions should ideally not happen.
+                });
+                //Скрываем кнопку "Завершить"
+                this.scanCloseShow = false;
             }
         }
         
